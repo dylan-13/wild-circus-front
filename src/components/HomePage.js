f3a4804196346aecdaabc74aef9e080b545c77dc
Dylan <dylan.berthier13@gmail.com>
2020-02-06 14:19:34 +0100

    css fixes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -43,8 +43,8 @@ function HomePage() {
 
     return (
         <div className="homepage">
-            <div className="shows">
-                <h2 id='shows'>Showmen</h2>
+            <div id='shows' className="shows">
+                <h2>Showmen</h2>
                 <br />
                 <img style={{ width: '100%', height: '100%', position: 'absolute', zIndex: '-1' }} src={homeBack} alt='backgroud' />
                 <h5>At the Wild circus Marseille, you can find some funny guys to laught with, Here is some of the showmen you should see</h5>
